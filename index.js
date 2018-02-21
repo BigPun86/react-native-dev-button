140582fbfc53524c312bbec03811f0d2ee84b24d
Adel Bruno Grimm <adel.grimm@icloud.com>
2018-02-21 13:00:18 +0100

    - Reformat code

--- index.js
+++ index.js
@@ -1,80 +1,85 @@
-import React, { PureComponent } from 'react';
-import { Alert, AsyncStorage, TouchableOpacity, View } from 'react-native';
-import Style from './styles';
+import React, { PureComponent } from "react";
+import { Alert, AsyncStorage, TouchableOpacity, View } from "react-native";
+import Style from "./styles";
 export default class DevButton extends PureComponent {
-  devActions = () => {
-    try {
-      const {
-        additionalActions,
-        alertMessage,
-        alertTitle,
-        clearActions
-      } = this.props;
-      const actions = [
-        {
-          text: 'Clear Storage!',
-          onPress: async () => {
-            await AsyncStorage.clear();
-            if (clearActions) {
-              await clearActions();
+    devActions = () => {
+        try {
+            const {
+                additionalActions,
+                alertMessage,
+                alertTitle,
+                clearActions
+            } = this.props;
+            const actions = [
+                {
+                    text: "Clear Storage!",
+                    onPress: async () => {
+                        await AsyncStorage.clear();
+                        if (clearActions) {
+                            await clearActions();
+                        }
+                        alert("Cleared AsyncStorage!");
+                    }
+                }
+            ];
+            if (additionalActions.length > 0) {
+                additionalActions.map((action, index) => {
+                    actions.push(action);
+                });
             }
-            alert('Cleared AsyncStorage!');
-          }
+            actions.push({ text: "Cancel" });
+            Alert.alert(alertTitle, alertMessage, actions);
+        } catch (error) {
+            console.log(error);
         }
-      ];
-      if (additionalActions.length > 0) {
-        additionalActions.map((action, index) => {
-          actions.push(action);
-        });
-      }
-      actions.push({ text: 'Cancel' });
-      Alert.alert(alertTitle, alertMessage, actions);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
 
-  render() {
-    if (!__DEV__) {
-      return null;
+    render() {
+        if (!__DEV__) {
+            return null;
+        }
+        const { position, styles } = this.props;
+        let containerStyle = Style.topContainer;
+        if (position) {
+            containerStyle =
+                (position === "topLeft" && Style.topLeftContainer) ||
+                (position === "topRight" && Style.topRightContainer) ||
+                (position === "bottomLeft" && Style.bottomLeftContainer) ||
+                (position === "bottomRight" && Style.bottomRightContainer);
+        }
+        return (
+            <TouchableOpacity
+                style={[containerStyle, styles]}
+                onPress={this.devActions}
+            >
+                <View style={[Style.roundShape]} />
+            </TouchableOpacity>
+        );
     }
-    const { position, styles } = this.props;
-    let containerStyle = Style.topContainer;
-    if (position) {
-      containerStyle =
-        (position === 'topLeft' && Style.topLeftContainer) ||
-        (position === 'topRight' && Style.topRightContainer) ||
-        (position === 'bottomLeft' && Style.bottomLeftContainer) ||
-        (position === 'bottomRight' && Style.bottomRightContainer);
-    }
-    return (
-      <TouchableOpacity
-        style={[containerStyle, styles]}
-        onPress={this.devActions}
-      >
-        <View style={[Style.roundShape]} />
-      </TouchableOpacity>
-    );
-  }
 }
 
-const PropTypes = require('prop-types');
+const PropTypes = require("prop-types");
 DevButton.propTypes = {
-  alertTitle: PropTypes.string,
-  alertMessage: PropTypes.string,
-  position: PropTypes.oneOf('topLeft', 'topRight', 'bottomLeft', 'bottomRight'),
-  clearActions: PropTypes.func,
-  additionalActions: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string,
-      onPress: PropTypes.func
-    })
-  )
+    alertTitle: PropTypes.string,
+    alertMessage: PropTypes.string,
+    position: PropTypes.oneOf(
+        "topLeft",
+        "topRight",
+        "bottomLeft",
+        "bottomRight"
+    ),
+    clearActions: PropTypes.func,
+    additionalActions: PropTypes.arrayOf(
+        PropTypes.shape({
+            text: PropTypes.string,
+            onPress: PropTypes.func
+        })
+    )
 };
 DevButton.defaultProps = {
-  alertTitle: 'Dev actions',
-  alertMessage: 'Clear storage or trigger any individually added dev action',
-  position: 'topLeft',
-  clearActions: () => {},
-  additionalActions: []
+    alertTitle: "Dev actions",
+    alertMessage: "Clear storage or trigger any individually added dev action",
+    position: "topLeft",
+    clearActions: () => {},
+    additionalActions: []
 };
